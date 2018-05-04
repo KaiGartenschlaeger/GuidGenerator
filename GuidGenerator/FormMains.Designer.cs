7418f5d6c81f564d1ca4fb75d1381edd0f7a88b1
Kai <mail@kaisnet.de>
2018-05-04 22:20:47 +0200

    Initial version

---
+++ GuidGenerator/FormMains.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace GuidGenerator
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.nudCount = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbxGuids = new System.Windows.Forms.ListBox();
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.cbxFormat = new System.Windows.Forms.ComboBox();
+            this.chbRemoveCopied = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.nudCount)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCreate.Location = new System.Drawing.Point(532, 31);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(80, 28);
+            this.btnCreate.TabIndex = 0;
+            this.btnCreate.Text = "Create";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // nudCount
+            // 
+            this.nudCount.Location = new System.Drawing.Point(65, 12);
+            this.nudCount.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudCount.Name = "nudCount";
+            this.nudCount.Size = new System.Drawing.Size(104, 20);
+            this.nudCount.TabIndex = 1;
+            this.nudCount.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Count";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Format";
+            // 
+            // lbxGuids
+            // 
+            this.lbxGuids.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbxGuids.FormattingEnabled = true;
+            this.lbxGuids.IntegralHeight = false;
+            this.lbxGuids.Location = new System.Drawing.Point(12, 72);
+            this.lbxGuids.Name = "lbxGuids";
+            this.lbxGuids.Size = new System.Drawing.Size(600, 314);
+            this.lbxGuids.TabIndex = 5;
+            this.lbxGuids.SelectedIndexChanged += new System.EventHandler(this.lbxGuids_SelectedIndexChanged);
+            this.lbxGuids.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.lbxGuids_MouseDoubleClick);
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCopy.Location = new System.Drawing.Point(532, 401);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(80, 28);
+            this.btnCopy.TabIndex = 6;
+            this.btnCopy.Text = "Copy";
+            this.btnCopy.UseVisualStyleBackColor = true;
+            this.btnCopy.Click += new System.EventHandler(this.btnCopy_Click);
+            // 
+            // cbxFormat
+            // 
+            this.cbxFormat.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cbxFormat.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbxFormat.FormattingEnabled = true;
+            this.cbxFormat.Location = new System.Drawing.Point(65, 38);
+            this.cbxFormat.Name = "cbxFormat";
+            this.cbxFormat.Size = new System.Drawing.Size(440, 21);
+            this.cbxFormat.TabIndex = 7;
+            // 
+            // chbRemoveCopied
+            // 
+            this.chbRemoveCopied.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.chbRemoveCopied.AutoSize = true;
+            this.chbRemoveCopied.Checked = true;
+            this.chbRemoveCopied.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chbRemoveCopied.Location = new System.Drawing.Point(12, 392);
+            this.chbRemoveCopied.Name = "chbRemoveCopied";
+            this.chbRemoveCopied.Size = new System.Drawing.Size(182, 17);
+            this.chbRemoveCopied.TabIndex = 8;
+            this.chbRemoveCopied.Text = "Remove selected guid after copy";
+            this.chbRemoveCopied.UseVisualStyleBackColor = true;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(624, 441);
+            this.Controls.Add(this.chbRemoveCopied);
+            this.Controls.Add(this.cbxFormat);
+            this.Controls.Add(this.btnCopy);
+            this.Controls.Add(this.lbxGuids);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.nudCount);
+            this.Controls.Add(this.btnCreate);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(640, 480);
+            this.Name = "FormMain";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "GuidGenerator";
+            ((System.ComponentModel.ISupportInitialize)(this.nudCount)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.NumericUpDown nudCount;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox lbxGuids;
+        private System.Windows.Forms.Button btnCopy;
+        private System.Windows.Forms.ComboBox cbxFormat;
+        private System.Windows.Forms.CheckBox chbRemoveCopied;
+    }
+}
+
---
+++ GuidGenerator/FormMains.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace GuidGenerator
+{
+    public partial class FormMain : Form
+    {
+        #region Constructor
+
+        public FormMain()
+        {
+            InitializeComponent();
+            InitializeFormats();
+            RefreshGuiState();
+        }
+
+        #endregion
+
+        #region Helper
+
+        private void InitializeFormats()
+        {
+            cbxFormat.DisplayMember = "Item1";
+
+            cbxFormat.Items.Add(new Tuple<string, string>("32 digits", "N"));
+            cbxFormat.Items.Add(new Tuple<string, string>("32 digits - separated by hyphens", "D"));
+
+            cbxFormat.Items.Add(new Tuple<string, string>("32 digits - separated by hyphens, enclosed in braces", "B"));
+            cbxFormat.SelectedIndex = 2;
+
+            cbxFormat.Items.Add(new Tuple<string, string>("32 digits - separated by hyphens, enclosed in parentheses", "P"));
+            cbxFormat.Items.Add(new Tuple<string, string>("Four hexadecimal values enclosed in braces, where the fourth value is a subset of eight hexadecimal values that is also enclosed in braces", "X"));
+        }
+
+        private void RefreshGuiState()
+        {
+            btnCopy.Enabled = lbxGuids.SelectedIndices.Count > 0;
+        }
+
+        private void CopyToClipboard()
+        {
+            if (lbxGuids.SelectedIndex == -1)
+                return;
+
+            Clipboard.SetText(lbxGuids.SelectedItem.ToString());
+
+            if (chbRemoveCopied.Checked)
+                lbxGuids.Items.RemoveAt(lbxGuids.SelectedIndex);
+
+            RefreshGuiState();
+
+            MessageBox.Show("The selected guid was copied to the clipboard.", "Info",
+                MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        #endregion
+
+        #region Control events
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            lbxGuids.BeginUpdate();
+            lbxGuids.Items.Clear();
+
+            var count = (int)nudCount.Value;
+            for (int i = 0; i < count; i++)
+            {
+                var guid = Guid.NewGuid();
+                var guidText = guid.ToString(((Tuple<string, string>)cbxFormat.SelectedItem).Item2);
+
+                lbxGuids.Items.Add(guidText);
+            }
+
+            lbxGuids.SelectedIndex = 0;
+            lbxGuids.EndUpdate();
+        }
+
+        private void btnCopy_Click(object sender, EventArgs e)
+        {
+            CopyToClipboard();
+        }
+
+        private void lbxGuids_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            CopyToClipboard();
+        }
+
+        private void lbxGuids_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            RefreshGuiState();
+        }
+
+        #endregion
+    }
+}
